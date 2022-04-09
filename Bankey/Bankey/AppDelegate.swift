42d8f95f5ecdd2aa59777fa1ea158d46262f28e8
Christopher Koski <ckoski18@gmail.com>
2022-04-09 17:18:52 -0400

    implented delegate protocol for onboarding & loginview

--- Bankey/Bankey/AppDelegate.swift
+++ Bankey/Bankey/AppDelegate.swift
@@ -9,18 +9,53 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+  
   var window: UIWindow?
+  
+  let loginViewController = LoginViewController()
+  let onboardingContainerViewController = OnboardingContainerViewController()
   
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
       
     window = UIWindow(frame: UIScreen.main.bounds)
     window?.makeKeyAndVisible()
     window?.backgroundColor = .systemBackground
-//    window?.rootViewController = LoginViewController()
-    window?.rootViewController = OnboardingContainerViewController()
+    
+    loginViewController.delegate = self
+    onboardingContainerViewController.delegate = self
+    
+    window?.rootViewController = loginViewController
+//    window?.rootViewController = onboardingContainerViewController
     
     return true
   }
 
+}
+
+extension AppDelegate: LoginViewControllerDelegate {
+  func didLogin() {
+    setRootViewController(onboardingContainerViewController)
+  }
+}
+
+extension AppDelegate: OnboardingContainerViewControllerDelegate {
+  func didFinishOnboarding() {
+    print("foo - Did finish")
+  }
+}
+
+extension AppDelegate {
+  
+  func setRootViewController(_ vc: UIViewController, animated: Bool = true) {
+    guard animated, let window = self.window else {
+      self.window?.rootViewController = vc
+      self.window?.makeKeyAndVisible()
+      return
+    }
+    
+    window.rootViewController = vc
+    window.makeKeyAndVisible()
+    UIView.transition(with: window, duration: 0.5, options: .transitionCrossDissolve, animations: nil, completion: nil)
+  }
 }
 
